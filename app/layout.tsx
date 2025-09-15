68917be2a0f8ba849d74d7c151762dcca84f1f99
ssmunkhbat <47767779+ssmunkhbat@users.noreply.github.com>
2025-09-15 10:45:48 +0800

    added pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { Providers } from "./providers";
 
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
+import { Head } from "next/document";
 
 export const metadata: Metadata = {
   title: {
@@ -32,9 +33,27 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+	const isProd = process.env.NODE_ENV === 'production'
   return (
     <html suppressHydrationWarning lang="en">
-      <head />
+      <head>
+        <meta charSet='utf-8' />
+        <link rel='icon' type='image/png' href={isProd ? `/next-pwa/images/favicon.png` : '/images/favicon.png'} />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover'
+        />
+        <meta
+          name='theme-color'
+          content='#18181b'
+          media='(prefers-color-scheme: dark)'
+        />
+        <meta name='theme-color' content='#f4f4f5' />
+        <link rel='apple-touch-icon' href={isProd ? '/next-pwa/images/icon-maskable-512.png' : '/images/icon-maskable-512.png'} />
+        <link rel='manifest' href={isProd ? `/next-pwa/manifest.json` : '/manifest.json'} />
+        {/* <script id="wepp-install-modal" src={isProd ? '/next-pwa/install/index.js' : '/install/index.js'} defer></script> */}
+        <script id="wepp-install-modal" src={isProd ? '/next-pwa/main.js' : '/main.js'} defer></script>
+      </head>
       <body
         className={clsx(
           "min-h-screen text-foreground bg-background font-sans antialiased",
--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	register: true,
+	skipWaiting: true,
+})
+
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'eas-system'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  trailingSlash: true,
+}
+
+module.exports = {...withPWA(nextConfig)}
