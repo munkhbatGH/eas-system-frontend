fff0acc9ba6595c881716a2d59b09db94a9fc7a5
ssmunkhbat <47767779+ssmunkhbat@users.noreply.github.com>
2025-09-23 17:33:57 +0800

    added settings

---
+++ app/admin/settings/module/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { title } from "@/components/primitives";
+import axios from "@/lib/axios";
+import { useEffect } from "react";
+
+export default function Profile() {
+  
+  useEffect(() => {
+    list()
+  }, []);
+
+  const list = async () => {
+    try {
+      const data = (await axios.get('/schema/list')).data
+      console.log('--module--list-----', data)
+    } catch (error) {
+      console.log('Error during login:', error)
+    }
+  };
+
+  return (
+    <div>
+      <h1 className={title()}>Module</h1>
+    </div>
+  );
+}
--- components/layout/sidebar/index.tsx
+++ components/layout/sidebar/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState } from "react";
-import { Home, Settings } from "lucide-react";
+import { useEffect, useState } from "react";
+import { ChevronDown, ChevronRight, Home, Link, Settings } from "lucide-react";
 
 import { useSystemStore } from "@/stores/systemStore";
 
@@ -12,57 +12,18 @@ export const Sidebar = () => {
   const toggleMenu = (menu: string) => {
     setOpenMenu(openMenu === menu ? null : menu);
   };
+
+  useEffect(() => {
+    console.log('----openMenu-----', openMenu, openMenu === "settings")
+  }, [openMenu]);
+
   return (
-    //   <aside className="max-sm:hidden w-64 border-r shadow-md p-4">
-    //     <h2 className="text-xl font-bold mb-6">Admin Panel</h2>
-
-    //     <Link href="/dashboard" className="block px-3 py-2 rounded hover:bg-gray-700">
-    //       Dashboard
-    //     </Link>
-
-    //     <button
-    //       onClick={() => toggleMenu("users")}
-    //       className="flex items-center justify-between w-full px-3 py-2 rounded hover:bg-gray-700"
-    //     >
-    //       <span>Users</span>
-    //       {openMenu === "users" ? <ChevronDown size={18} /> : <ChevronRight size={18} />}
-    //     </button>
-    //     {openMenu === "users" && (
-    //       <div className="ml-6 space-y-1">
-    //         <Link href="/users/list" className="block px-3 py-1 hover:bg-gray-700 rounded">
-    //           List
-    //         </Link>
-    //         <Link href="/users/create" className="block px-3 py-1 hover:bg-gray-700 rounded">
-    //           Create
-    //         </Link>
-    //       </div>
-    //     )}
-
-    //     <button
-    //       onClick={() => toggleMenu("settings")}
-    //       className="flex items-center justify-between w-full px-3 py-2 rounded hover:bg-gray-700"
-    //     >
-    //       <span>Settings</span>
-    //       {openMenu === "settings" ? <ChevronDown size={18} /> : <ChevronRight size={18} />}
-    //     </button>
-    //     {openMenu === "settings" && (
-    //       <div className="ml-6 space-y-1">
-    //         <Link href="/settings/profile" className="block px-3 py-1 hover:bg-gray-700 rounded">
-    //           Profile
-    //         </Link>
-    //         <Link href="/settings/security" className="block px-3 py-1 hover:bg-gray-700 rounded">
-    //           Security
-    //         </Link>
-    //       </div>
-    //     )}
-    // </aside>
-
     <aside id="sidebar" className={`max-sm:hidden border-r dark:border-gray-600 border-gray-300 flex flex-col justify-start items-center transition-all duration-300 ${expanded ? "w-64" : "w-16"}`}>
 
       <nav className={`flex-1 mt-4 space-y-2 ${expanded ? "w-full" : ""}`}>
         <a
           href="/dashboard"
-          className={`flex items-center py-3 px-8 rounded hover:bg-gray-700 transition-colors text-inherit  ${expanded ? "gap-2" : ""}`}
+          className={`w-full flex items-center py-3 px-8 rounded hover:bg-gray-700 transition-colors text-inherit  ${expanded ? "gap-2" : ""}`}
         >
           <Home size={20} />
           <span
@@ -72,19 +33,33 @@ export const Sidebar = () => {
             Dashboard
           </span>
         </a>
-
+        
         <a
-          href="/settings"
+          href="#"
+          onClick={() => toggleMenu("settings")}
           className={`flex items-center py-3 px-8 rounded hover:bg-gray-700 transition-colors text-inherit ${expanded ? "gap-2" : ""}`}
         >
           <Settings size={20} />
-          <span
-            className={` overflow-hidden whitespace-nowrap transition-all duration-300 
-            ${expanded ? "opacity-100 max-w-xs" : "opacity-0 max-w-0"}`}
-          >
-            Settings
-          </span>
+          <span>Тохиргоо</span>
+          {openMenu === "settings" ? <ChevronDown size={20} /> : <ChevronRight size={20} />}
         </a>
+        {openMenu === "settings" && (
+          <div className="ml-6 space-y-1">
+            <a
+              href="/admin/settings/module"
+              className={`flex items-center py-3 px-8 rounded hover:bg-gray-700 transition-colors text-inherit ${expanded ? "gap-2" : ""}`}
+            >
+              <span>Модуль</span>
+            </a>
+            <a
+              href="/admin/settings/menu"
+              className={`flex items-center py-3 px-8 rounded hover:bg-gray-700 transition-colors text-inherit ${expanded ? "gap-2" : ""}`}
+            >
+              <span>Цэс</span>
+            </a>
+          </div>
+        )}
+
       </nav>
     </aside>
   );
