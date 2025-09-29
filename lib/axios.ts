47cf07c4221c85eb4aba903db0bb9e7a608843f5
ssmunkhbat <47767779+ssmunkhbat@users.noreply.github.com>
2025-09-29 12:42:54 +0800

    aaa

--- lib/axios.ts
+++ lib/axios.ts
@@ -7,6 +7,11 @@ const token_name = process.env.NEXT_PUBLIC_TOKEN || 'eas-token';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': 'true',
+  },
 });
 
 // Add request interceptor
